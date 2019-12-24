981015aab81c992d48a264139364b624ac070fef
Thomas Duchatelet <thomas.duchatelet.y3284@student.hogent.be>
2019-12-24 12:29:42 +0100

    Added index view

--- SnakeEyesGame/Controllers/HomeController.cs
+++ SnakeEyesGame/Controllers/HomeController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SnakeEyesGame.Models;
 
 namespace SnakeEyesGame.Controllers
 {
     public class HomeController : Controller
     {
+        private SnakeEyes _snakeEyes;
         public IActionResult Index()
         {
-            return View();
+            _snakeEyes = new SnakeEyes();
+            return View(_snakeEyes);
         }
     }
 }
