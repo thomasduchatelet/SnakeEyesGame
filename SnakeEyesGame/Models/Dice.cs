a30c9baa3c21158880a29dda864f497cb25ccc66
Thomas Duchatelet <thomas.duchatelet.y3284@student.hogent.be>
2019-12-24 11:52:57 +0100

    implemented models

---
+++ SnakeEyesGame/Models/Dice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnakeEyesGame.Models
+{
+    public class Dice
+    {
+
+        #region Fields
+        private static Random _random = new Random();
+        #endregion
+
+        #region Properties
+        public int Pips { get; private set; }
+        #endregion
+
+        #region Constructors
+        public Dice()
+        {
+            _random = new Random();
+            Pips = 6;
+        }
+        #endregion
+
+        #region Methods
+        public void Roll() 
+        {
+            Pips = _random.Next(1, 7);
+        }
+        #endregion
+    }
+}
---
+++ SnakeEyesGame/Models/SnakeEyes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnakeEyesGame.Models
+{
+    public class SnakeEyes
+    {
+        #region Fields
+        private readonly Dice _eye1;
+        private readonly Dice _eye2;
+        #endregion
+
+        #region Properties
+        public int Eye1 => _eye1.Pips;
+        public int Eye2 => _eye2.Pips;
+        public int Total { get; private set; }
+        #endregion
+
+        #region Constructors
+        public SnakeEyes()
+        {
+            _eye1 = new Dice();
+            _eye2 = new Dice();
+            Total = 0;
+        }
+        #endregion
+
+        #region Methods
+        public void Play()
+        {
+            _eye1.Roll();
+            _eye2.Roll();
+            Total += Eye1 + Eye2;
+        }
+        #endregion
+    }
+}
